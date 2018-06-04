dd2ede2a28ccd42a88e2ae3322db75eb5d9c731a
Bjorn Stange <bjorn248@gmail.com>
2018-06-04 19:06:10 -0400

    testing lambda-based user creation

--- user_registration.go
+++ user_registration.go
@@ -21,8 +21,31 @@ type Response struct {
 	Message string `json:"Response"`
 }
 
-// HandleRequest processes the creation of the cognito user
+// HandleRequest is the lambda function handler
+// it processes the creation of the cognito user
 func HandleRequest(ctx context.Context, evt UserRegEvent) (Response, error) {
+	var cognitoAppClientID string
+	if os.Getenv("COGNITO_APP_CLIENT_ID") == "" {
+		log.Fatal("COGNITO_APP_CLIENT_ID not set")
+	} else {
+		cognitoAppClientID = os.Getenv("COGNITO_APP_CLIENT_ID")
+	}
+
+	sess := session.Must(session.NewSession())
+
+	cognitoService := cognitoidentityprovider.New(sess)
+
+	cognitoInput := cognitoidentityprovider.SignUpInput{
+		ClientId: &cognitoAppClientID,
+		Username: &evt.Username,
+		Password: &evt.Password,
+	}
+
+	cognitoResponse, err := cognitoService.SignUp(&cognitoInput)
+	if err != nil {
+		return Response{Message: "Error creating user: "}, err
+	}
+	fmt.Println(cognitoResponse)
 	return Response{Message: fmt.Sprintf("Successfully created user %s", evt.Username)}, nil
 }
 
@@ -36,32 +59,6 @@ func main() {
 	if os.Getenv("AWS_REGION") == "" {
 		log.Fatal("AWS_REGION not set")
 	}
-
-	var cognitoAppClientID string
-	if os.Getenv("COGNITO_APP_CLIENT_ID") == "" {
-		log.Fatal("COGNITO_APP_CLIENT_ID not set")
-	} else {
-		cognitoAppClientID = os.Getenv("COGNITO_APP_CLIENT_ID")
-	}
-
-	username := "bjorn248@gmail.com"
-	password := "Test1234$"
-
-	sess := session.Must(session.NewSession())
-
-	cognitoService := cognitoidentityprovider.New(sess)
-
-	cognitoInput := cognitoidentityprovider.SignUpInput{
-		ClientId: &cognitoAppClientID,
-		Username: &username,
-		Password: &password,
-	}
-
-	cognitoResponse, err := cognitoService.SignUp(&cognitoInput)
-	if err != nil {
-		log.Fatal("Error signing up cognito user:", err)
-	}
-	fmt.Println(cognitoResponse)
 
 	lambda.Start(HandleRequest)
 }
